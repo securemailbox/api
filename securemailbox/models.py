78e45e8337244ef67d91e9deead01879fc08d482
Sam Gomena <sgomena@tripwire.com>
2020-02-20 21:07:57 -0800

    fix: Add __repr__ for models, comments for docs

--- securemailbox/models.py
+++ securemailbox/models.py
@@ -1,22 +1,34 @@
+from datetime import datetime
 
-from securemailbox import db
+from . import db
 
 # TODO: Determine what a good fingerprint length is
 FINGERPRINT_LENGTH = 100
 
-class Mailbox(db.model):
+class Mailbox(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fingerprint = db.Column(db.String(FINGERPRINT_LENGTH), unique=True)
     is_active = db.Column(db.Boolean)
     
+    # Enable datetime support with `True`
+    # Docs: https://docs.sqlalchemy.org/en/13/core/type_basics.html#sqlalchemy.types.DateTime.__init__
     created_at = db.Column(db.DateTime(True), default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(True), default=datetime.utcnow)
 
-class Message(db.model):
+    def __repr__(self):
+        return '<Mailbox %r>' % self.fingerprint
+
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.String(1000))
+    message = db.Column(db.Text)
     sender_fingerprint = db.Column(db.String(FINGERPRINT_LENGTH))
 
     # TODO: Determine relationship to mailbox model
+
+    # Enable datetime support with `True`
+    # Docs: https://docs.sqlalchemy.org/en/13/core/type_basics.html#sqlalchemy.types.DateTime.__init__
     created_at = db.Column(db.DateTime(True), default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(True), default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message %r>' % self.message
