ccec31f0f98a886424b37b82d5c75113c188e4a9
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-02-26 16:42:13 -0800

    updated retrieve endpoint to example

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from ..models import Mailbox
 
 from securemailbox import db
 
@@ -8,4 +10,12 @@ retrieve_blueprint = Blueprint("retrieve", __name__)
 
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
-    return jsonify({"success": True, "error": None})
+
+    fingerprint = request.json.get("fingerprint", None)
+    if fingerprint is None:
+        return jsonify({"error": "fingerprint is none"}), 400
+
+    mailbox = Mailbox.query.filter_by(fingerprint=fingerprint)
+
+    messages = str(...)
+    return jsonify({"success": True, messages: messages, "error": None})
