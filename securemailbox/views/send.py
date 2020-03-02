be4b1dcd233b1bcdc3a6dfae97c1c10762c99c06
Tom3424 <44450996+Tom3424@users.noreply.github.com>
2020-03-02 14:30:20 -0800

    Update send.py

--- securemailbox/views/send.py
+++ securemailbox/views/send.py
@@ -1,4 +1,8 @@
+
 from flask import Blueprint, jsonify
+
+from ..models import Mailbox
+from ..models import Message
 
 from securemailbox import db
 
@@ -8,4 +12,20 @@ send_blueprint = Blueprint("send", __name__)
 
 @send_blueprint.route("/send/", methods=["POST"])
 def send():
+
+    #get fingerprint/check if can retrieve.validity
+    fingerprint = request.json.get("fingerprint", None)
+    if fingerprint is None:
+        return jsonify({"error": "no fingerprint"}), 400
+    #update messages associated
+    #not sure how to make the id unique and get current user fingerprint? clarification
+    #needed/retrieve all ids to check for available? counter?
+    #message not retrieved for testing purposes.
+    message = Message(id = 1, message="test1",sender_fingerprint=fingerprint, mailbox_id=1)
+    db.session.add(message)
+    db.session.commit()
+    
     return jsonify({"success": True, "error": None})
+
+    
+    #return jsonify({"success": True, "error": None})
