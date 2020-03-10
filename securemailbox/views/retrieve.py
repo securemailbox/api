c206026f365c47841e465884dd54c8a25cf8eca9
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-03-09 20:28:29 -0700

    more testing stuff

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -10,6 +10,39 @@ retrieve_blueprint = Blueprint("retrieve", __name__)
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
 
+
+ 	# fingerprint = request.json.get("fingerprint", None)
+    # fingerprint = request.get_json
+
+    
+    # if request.json is None:
+    # 	return jsonify({"Error": True})
+
+    fingerprint = request.get_json("fingerprint")
+    print(fingerprint)
+    # if fingerprint is None:
+        # return jsonify({'error': True}), 400
+
+    # print(request.method)
+
+    # print(request.is_json)
+
+    # content = request.json
+    # print (content)
+    # print()
+
+    fingerprint = request.json.get("fingerprint", None)
+    extradata = request.json.get("extradata", None)
+
+
+    if fingerprint is not None and extradata is not None:
+    	return jsonify({"fingerprint": fingerprint, "extradata": extradata})
+
+
+    # return jsonify({"success": True})
+
+
+
     # tables = db.get_tables_for_bind()
     # mailtest = tables[0](fingerprint='oihjuhqfd', is_active=True)
     # db.session.add(mailtest)
@@ -34,26 +67,3 @@ def retrieve():
 
     # messages = str(...)
     # return jsonify({"success": True, messages: messages, "error": None})
-    ##############################################################
-
-    # fingerprint = request.json.get("fingerprint", None)
-    # fingerprint = request.get_json
-    fingerprint = request.get_json("fingerprint")
-    print(fingerprint)
-    # if fingerprint is None:
-        # return jsonify({'error': True}), 400
-
-    print(request.method)
-
-    print(request.data)
-
-    print(request.args)
-
-    print(request.form)
-
-    print(request.is_json)
-
-    content = request.json
-    print (content)
-    print()
-    return jsonify({"success": True})
