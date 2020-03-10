e600c3c51cfacd673e724990430bfcb5427ea631
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-03-09 17:54:16 -0700

    testing json requests

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, json
 
 from ..models import Mailbox
 
@@ -7,15 +7,53 @@ from securemailbox import db
 # Create the `retrieve` blueprint
 retrieve_blueprint = Blueprint("retrieve", __name__)
 
-
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
 
-    fingerprint = request.json.get("fingerprint", None)
-    if fingerprint is None:
-        return jsonify({"error": "fingerprint is none"}), 400
+    # tables = db.get_tables_for_bind()
+    # mailtest = tables[0](fingerprint='oihjuhqfd', is_active=True)
+    # db.session.add(mailtest)
+    # db.session.commit()
+    # return jsonify({"mail1": False})
 
-    mailbox = Mailbox.query.filter_by(fingerprint=fingerprint)
+    # return jsonify({"success": True, "error": None})
 
-    messages = str(...)
-    return jsonify({"success": True, messages: messages, "error": None})
+    # print (request.is_json)
+    # content = request.get_json()
+    # print (content)
+    # return 'JSON posted'
+    
+    # fingerprint = request.get_json("fingerprint", None)#("fingerprint", None)
+    # print(fingerprint)
+    # # if fingerprint == 12:
+    # #     print('yep')
+    # if fingerprint is None:
+    #     return jsonify({"error": "fingerprint is none"}), 400
+
+    # mailbox = Mailbox.query.filter_by(fingerprint=fingerprint)
+
+    # messages = str(...)
+    # return jsonify({"success": True, messages: messages, "error": None})
+    ##############################################################
+
+    # fingerprint = request.json.get("fingerprint", None)
+    # fingerprint = request.get_json
+    fingerprint = request.get_json("fingerprint")
+    print(fingerprint)
+    # if fingerprint is None:
+        # return jsonify({'error': True}), 400
+
+    print(request.method)
+
+    print(request.data)
+
+    print(request.args)
+
+    print(request.form)
+
+    print(request.is_json)
+
+    content = request.json
+    print (content)
+    print()
+    return jsonify({"success": True})
