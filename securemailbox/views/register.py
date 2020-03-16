40614f9326f46365beda39ab3c805d4d9c2311c9
samgomena <samgomena@protonmail.com>
2020-03-15 17:52:11 -0700

    feat: Add initial register endpoint

--- securemailbox/views/register.py
+++ securemailbox/views/register.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from securemailbox import db
+from ..models import Mailbox
 
 # Create the `register` blueprint
 # Docs:
@@ -9,4 +10,29 @@ register_blueprint = Blueprint("register", __name__)
 
 @register_blueprint.route("/register/", methods=["POST"])
 def register():
-    return jsonify({"success": True, "error": None})
+    # Ensure that the request is valid json
+    if not request.is_json():
+        return jsonify({"success": False, "error": "Request must be valid json"}), 400
+
+    # Ensure that all required fields are present in the request body
+    for field in ["fingerprint"]:
+        if request.get(field, None) is None:
+            return (
+                jsonify({"success": False, "error": f"field '{field}' is required."}),
+                400,
+            )
+
+    try:
+        fingerprint = request.json.get("fingerprint")
+
+        new_mailbox = Mailbox(fingerprint=fingerprint)
+        db.session.add(new_mailbox)
+        db.session.commit()
+
+        return (
+            jsonify({"success": True, "error": None, data: {"mailbox": new_mailbox}}),
+            200,
+        )
+    except BaseException as e:
+        # There was an unknown error
+        return jsonify({"success": False, "error": e}), 500
