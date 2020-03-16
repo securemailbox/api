554927cd0813cf1b94bcfb4fbf50390c89698577
samgomena <samgomena@protonmail.com>
2020-03-15 18:41:55 -0700

    feat: Add initial retrieve endpoint functionality

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -10,12 +10,39 @@ retrieve_blueprint = Blueprint("retrieve", __name__)
 
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
+    # Ensure that the request is valid json
+    if not request.is_json:
+        return jsonify({"success": False, "error": "Request must be valid json"}), 400
 
-    fingerprint = request.json.get("fingerprint", None)
-    if fingerprint is None:
-        return jsonify({"error": "fingerprint is none"}), 400
+    # Ensure that all required fields are present in the request body
+    for field in ["fingerprint"]:
+        if request.json.get(field, None) is None:
+            return (
+                jsonify({"success": False, "error": f"field '{field}' is required."}),
+                400,
+            )
+    try:
+        fingerprint = request.json.get("fingerprint")
+        mailbox = Mailbox.query.filter_by(fingerprint=fingerprint).first()
+        if mailbox is None:
+            return (
+                jsonify(
+                    {
+                        "success": False,
+                        "error": f"mailbox assosciated with fingerprint '{fingerprint}' doesn't esixt",
+                    }
+                ),
+                404,
+            )
+        # Query all messages associated with the mailbox
+        # TODO: Determine what should be done with all the messages
+        # I.e. Are they deleted/archived? Are they marked as 'read'?
+        messages = mailbox.messages.all()
+        return (
+            jsonify({"success": True, "error": None, "data": {"messages": messages}}),
+            200,
+        )
 
-    mailbox = Mailbox.query.filter_by(fingerprint=fingerprint)
-
-    messages = str(...)
-    return jsonify({"success": True, messages: messages, "error": None})
+    # Catch all; there was an unknown error
+    except BaseException as e:
+        return jsonify({"success": False, "error": repr(e)}), 500
