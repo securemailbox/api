bb9d083de420fbafbf18f5ccea5101e0c573446d
Joseph Starr <starr5@pdx.edu>
2020-04-06 13:01:21 -0800

    pre test code

--- securemailbox/views/register.py
+++ securemailbox/views/register.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, jsonify, request, json
-from datetime import datetime
-from ..models import Mailbox
+from flask import Blueprint, jsonify, request
 
 from securemailbox import db
+from ..models import Mailbox
+from ..models import Message
 
 # Create the `register` blueprint
 # Docs:
@@ -12,22 +12,14 @@ register_blueprint = Blueprint("register", __name__)
 @register_blueprint.route("/register/", methods=["POST"])
 def register():
     incoming_mailbox = request.get_json(force=True, silent=True)
-    error = None
-    incoming_fingerprint = incoming_mailbox['fingerprint']
-    incoming_id = incoming_mailbox['id']
-    if incoming_fingerprint or incoming_id is None:
-        return jsonify({"success": False, "error": "missing required user information"})
 
+    if incoming_mailbox is None or incoming_mailbox['fingerprint'] is None:
+        return jsonify({"success": False, "error": ""})
+
+    fetched_mailboxes = Mailbox.query.all()
+    for fetched_mailbox in fetched_mailboxes:
+        if fetched_mailbox['fingerprint'] == incoming_mailbox['fingerprint']:
+            return jsonify({"success": False, "error": "fingerprint already exists"})
+    db.session.add(incoming_mailbox)
     return jsonify({"success": True, "error": None})
 
-# connected_db = db.get_db()
-    # if connected_db.execute(
-    #     'SELECT id FROM user WHERE fingerprint = ?', (fingerprint,)
-    # ).fetchnone() is None:
-    #     return jsonify({"success": False, "error": 'User is already registered.'})
-    # created_at = datetime.now()
-    # updated_at = datetime.now()
-    # if error is None:
-    #     connected_db.execute(
-    #         'INSERT INTO Mailbox (fingerprint'
-    #     )
