e2356a0df090447121ce8bf496157caafb0de470
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-04-06 20:04:58 -0700

    error check return statement

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -11,58 +11,21 @@ retrieve_blueprint = Blueprint("retrieve", __name__)
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
 
-    # #######################
-    # # Testing mailbox add
-    # mail = Mailbox(fingerprint='qwe')
-    # db.session.add(mail)
-    # mail = Mailbox(fingerprint='asd')
-    # db.session.add(mail)
-    # mail = Mailbox(fingerprint='zxc')
-    # db.session.add(mail)
-    # mail = Mailbox(fingerprint='bnm')
-    # db.session.add(mail)
-
-    # db.session.commit()
-
-    # # Testing message add
-    # mess = Message(message='message from qwe to asd', 
-    #     sender_fingerprint='qwe',
-    #     mailbox_id=2)
-    # db.session.add(mess)
-
-    # mess = Message(message='message from zxc to asd', 
-    #     sender_fingerprint='zxc',
-    #     mailbox_id=2)
-    # db.session.add(mess)
-
-    # mess = Message(message='message from bnm to asd', 
-    #     sender_fingerprint='bnm',
-    #     mailbox_id=2)
-    # db.session.add(mess)
-
-    # mess = Message(message='message from asd to qwe', 
-    #     sender_fingerprint='asd',
-    #     mailbox_id=1)
-    # db.session.add(mess)
-
-    # db.session.commit()
-    # #######################
-
     data = request.get_json(force=True, silent=True)
 
     if data is None:
-        return jsonify({"Invalid JSON": True}), 400
+        return jsonify({'error': 'Invalid JSON'}), 400
 
     # Check if fingerprint is present in json request body
     if 'fingerprint' not in data:
-        return jsonify({'fingerprint not in request': True}), 400
+        return jsonify({'error': 'fingerprint not in request'}), 400
 
     # search for fingerprint in mailbox table, assign to variable
     mailbox_num = Mailbox.query.filter_by(fingerprint=data['fingerprint']).first()
 
     # check if mailbox_num is a valid entry
     if mailbox_num is None:
-        return jsonify({'fingerprint not in database': True}), 400    
+        return jsonify({'error': 'fingerprint not in database'}), 400    
 
     # set mailbox_num to id attribute
     mailbox_num = mailbox_num.id
@@ -77,7 +40,7 @@ def retrieve():
     if sender_is_present:
         message_receive = Message.query.filter_by(mailbox_id=mailbox_num, sender_fingerprint=data['sender_fingerprint']).all()
         if len(message_receive) == 0:
-            return jsonify({'sender_fingerprint incorrect': True}), 400
+            return jsonify({'error': 'sender_fingerprint incorrect'}), 400
     else:
         message_receive = Message.query.filter_by(mailbox_id=mailbox_num).all()
 
