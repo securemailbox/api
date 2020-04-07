fb018c25f6384cdb99b179c23112953150b9caa4
Joseph Starr <starr5@pdx.edu>
2020-04-06 19:29:18 -0800

    register endpoint tested and is persisting data into database.

--- securemailbox/views/register.py
+++ securemailbox/views/register.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import IntegrityError
 
 from securemailbox import db
 from ..models import Mailbox
@@ -10,16 +11,37 @@ register_blueprint = Blueprint("register", __name__)
 
 @register_blueprint.route("/register/", methods=["POST"])
 def register():
-    incoming_mailbox = request.get_json(force=True, silent=True)
+    # Ensure a valid request is received
+    if not request.is_json:
+        return jsonify({"success": False, "error": "Request must be valid json"}), 400
 
-    if incoming_mailbox is None:
-        return jsonify({"success": False, "error": "No register data received."})
-    if incoming_mailbox['fingerprint'] is None:
-        return jsonify({"success": False, "error": "No fingerprint received."})
+    # Ensure all fields in request are not NULL
+    for field in ["fingerprint"]:
+        if request.json.get(field, None) is None:
+            return (
+                jsonify({"success": False, "error": f"field '{field}' is required."}),
+                400,
+            )
     try:
-        new_mailbox = Mailbox(incoming_mailbox)
+        incoming_fingerprint = request.json.get("fingerprint")
+        new_mailbox = Mailbox(fingerprint=incoming_fingerprint)
         db.session.add(new_mailbox)
-        return jsonify({"success": True, "error": None})
-    except IntegrityError:
-        return jsonify({"success": True, "error": "Mailbox already exists"})
+        db.session.commit()
 
+        return (
+            jsonify({"success": True, "error": None, "data": {"mailbox": new_mailbox.fingerprint}}),
+            200,
+        )
+    except IntegrityError:
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": f"mailbox with fingerprint '{incoming_fingerprint}' already exists",
+                }
+            ),
+            400,
+        )
+    # Catch all; there was an unknown error
+    except BaseException as e:
+        return jsonify({"success": False, "error": repr(e)}), 500
