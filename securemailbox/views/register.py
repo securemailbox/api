349a0b8c0093e81867b545a7e65586901c124ef4
Joseph Starr <starr5@pdx.edu>
2020-04-06 17:59:27 -0800

    changed query to get fingerprints to check if incoming mailbox exists to try except

--- securemailbox/views/register.py
+++ securemailbox/views/register.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 
 from securemailbox import db
 from ..models import Mailbox
-from ..models import Message
 
 # Create the `register` blueprint
 # Docs:
@@ -13,13 +12,14 @@ register_blueprint = Blueprint("register", __name__)
 def register():
     incoming_mailbox = request.get_json(force=True, silent=True)
 
-    if incoming_mailbox is None or incoming_mailbox['fingerprint'] is None:
-        return jsonify({"success": False, "error": ""})
-
-    fetched_mailboxes = Mailbox.query.all()
-    for fetched_mailbox in fetched_mailboxes:
-        if fetched_mailbox['fingerprint'] == incoming_mailbox['fingerprint']:
-            return jsonify({"success": False, "error": "fingerprint already exists"})
-    db.session.add(incoming_mailbox)
-    return jsonify({"success": True, "error": None})
+    if incoming_mailbox is None:
+        return jsonify({"success": False, "error": "No register data received."})
+    if incoming_mailbox['fingerprint'] is None:
+        return jsonify({"success": False, "error": "No fingerprint received."})
+    try:
+        new_mailbox = Mailbox(incoming_mailbox)
+        db.session.add(new_mailbox)
+        return jsonify({"success": True, "error": None})
+    except IntegrityError:
+        return jsonify({"success": True, "error": "Mailbox already exists"})
 
