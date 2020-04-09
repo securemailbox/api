3e2376c8c5e6dcfb99814c53c5067f0234f7d515
Cameron Pyne <campyne@pdx.edu>
2020-04-08 23:36:56 -0700

    fix: copypaste mistake

--- test_app.py
+++ test_app.py
@@ -16,4 +16,4 @@ def test_register(client):
         "/register/", json={"fingerprint": "FAC10F0C3D1D49F8F9A82CB553E79F7C92E1CF32"}
     )
     json_data = rv.get_json()
-    assert json_data == {"success": True, "error": None, "data": {"mailbox": new_mailbox.fingerprint}}
+    assert json_data == {"success": True, "error": None, "data": {"mailbox": "FAC10F0C3D1D49F8F9A82CB553E79F7C92E1CF32"}}
