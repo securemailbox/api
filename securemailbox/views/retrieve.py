ecedc5bc7e77f9b84893d600940ca86f60cdd471
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-04-10 14:13:12 -0700

    ran black formatter

--- securemailbox/views/retrieve.py
+++ securemailbox/views/retrieve.py
@@ -10,62 +10,105 @@ from securemailbox import db
 # Create the `retrieve` blueprint
 retrieve_blueprint = Blueprint("retrieve", __name__)
 
+
 @retrieve_blueprint.route("/retrieve/", methods=["POST"])
 def retrieve():
 
     # Check for valid json request
     if not request.is_json:
-        return jsonify({'success': False, 'error': 'Request must be valid json', 'data': None}), 400
+        return (
+            jsonify(
+                {"success": False, "error": "Request must be valid json", "data": None}
+            ),
+            400,
+        )
 
-    req_fingerprint = request.json.get('fingerprint', None)
+    req_fingerprint = request.json.get("fingerprint", None)
 
     # Check if fingerprint is present in json request body
     if req_fingerprint is None:
-        return jsonify({'success': False, 'error': 'fingerprint not in request', 'data': None}), 400
+        return (
+            jsonify(
+                {"success": False, "error": "fingerprint not in request", "data": None}
+            ),
+            400,
+        )
 
     # Check if fingerprint value is string variable
     if not isinstance(req_fingerprint, str):
-        return jsonify({'success': False, 'error': 'fingerprint value not valid', 'data': None}), 400
+        return (
+            jsonify(
+                {"success": False, "error": "fingerprint value not valid", "data": None}
+            ),
+            400,
+        )
 
     # search for fingerprint in mailbox table
     try:
         mailbox_id = Mailbox.query.filter_by(fingerprint=req_fingerprint).first()
     except NoResultFound:
-        return jsonify({'success': False, 'error': 'fingerprint not found in database', 'data': None}), 400
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": "fingerprint not found in database",
+                    "data": None,
+                }
+            ),
+            400,
+        )
 
     # check if mailbox_id is a valid entry before using it
     if mailbox_id is None:
-        return jsonify({'success': False, 'error': 'fingerprint not in database', 'data': None}), 400    
-    
-    # set mailbox_num to id attribute
+        return (
+            jsonify(
+                {"success": False, "error": "fingerprint not in database", "data": None}
+            ),
+            400,
+        )
+
+    # set mailbox_num to id attribute after checking it's not None
     mailbox_id = mailbox_id.id
-    
+
     # if sender_fingerprint provided, it is set, otherwise None
-    req_sender_fingerprint = request.json.get('sender_fingerprint', None)    
+    req_sender_fingerprint = request.json.get("sender_fingerprint", None)
 
     # query for messages differently if sender_fingerprint is provided
     try:
         if req_sender_fingerprint:
-            message_receive = Message.query.filter_by(mailbox_id=mailbox_id, sender_fingerprint=req_sender_fingerprint).all()
+            message_receive = Message.query.filter_by(
+                mailbox_id=mailbox_id, sender_fingerprint=req_sender_fingerprint
+            ).all()
         else:
             message_receive = Message.query.filter_by(mailbox_id=mailbox_id).all()
 
     except IntegrityError:
-        return jsonify({'success': False, 'error': 'Unknown error receiving messages', 'data': None}), 400
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": "Unknown error receiving messages",
+                    "data": None,
+                }
+            ),
+            400,
+        )
 
     except BaseException as e:
-        return jsonify({'success': False, 'error': repr(e), 'data': None}), 500
+        return jsonify({"success": False, "error": repr(e), "data": None}), 500
 
     # return all fields of all related messages
     all_messages = []
 
     for message in message_receive:
-        all_messages.append({
-            "message": message.message,
-            "sender_fingerprint": message.sender_fingerprint,
-            "created_at": message.created_at,
-            "updated_at": message.updated_at,
-        })
+        all_messages.append(
+            {
+                "message": message.message,
+                "sender_fingerprint": message.sender_fingerprint,
+                "created_at": message.created_at,
+                "updated_at": message.updated_at,
+            }
+        )
 
     return (
         jsonify(
@@ -74,5 +117,6 @@ def retrieve():
                 "error": None,
                 "data": {"messages": all_messages, "count": len(all_messages)},
             }
-        ), 200
+        ),
+        200,
     )
