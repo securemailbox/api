387aa508fb8add02045ad8cca4d4c0895925020c
campyne <32502547+campyne@users.noreply.github.com>
2020-04-21 16:51:15 -0700

    define SSH and SCP variables to clean it up a bit
    Co-Authored-By: Sam Gomena <samgomena@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,14 @@ elif [[ -z "$POSTGRES_DB" ]]; then
   echo "POSTGRES_DB not set"
   exit 1
 fi
+SSH="sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+SCP="sshpass -e scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null securemailbox.tar root@${DEPLOYMENT_TARGET}:/root/securemailbox.tar
-sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DEPLOYMENT_TARGET} "docker stop securemailbox"
-sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DEPLOYMENT_TARGET} "docker rm securemailbox"
-sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DEPLOYMENT_TARGET} "docker load -i securemailbox.tar"
-sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DEPLOYMENT_TARGET} "docker run -d --name securemailbox --network host -e DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} securemailbox:latest"
+${SCP} securemailbox.tar root@${DEPLOYMENT_TARGET}:/root/securemailbox.tar
+${SSH} root@${DEPLOYMENT_TARGET} "docker stop securemailbox"
+${SSH} root@${DEPLOYMENT_TARGET} "docker rm securemailbox"
+${SSH} root@${DEPLOYMENT_TARGET} "docker load -i securemailbox.tar"
+${SSH} root@${DEPLOYMENT_TARGET} "docker run -d --name securemailbox --network host -e DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} securemailbox:latest"
 sleep 2
 running=$( sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DEPLOYMENT_TARGET} "docker inspect -f {{.State.Running}} securemailbox" )
 if [[ $running = "true" ]]
