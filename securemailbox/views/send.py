c00b0781e5eae53122a58649d0e60626c4140607
samgomena <samgomena@protonmail.com>
2020-05-03 14:41:41 -0700

    feat: Update send docs

--- securemailbox/views/send.py
+++ securemailbox/views/send.py
@@ -22,13 +22,13 @@ def send():
     #get fingerprint
     fingerprint = request.json.get("fingerprint", None)
     if fingerprint is None:
-        return jsonify({"error": "no fingerprint"}), 400
+        return jsonify({"success": False, "error": "no fingerprint"}), 400
     sender_fingerprint = request.json.get("sender_fingerprint", None)
     if sender_fingerprint is None:
-        return jsonify({"error": "no send fingerprint"}), 400
+        return jsonify({"success": False, "error": "no send fingerprint"}), 400
     message_t = request.json.get("message", None)
     if message_t is None:
-        return jsonify({"error": "no message"}), 400
+        return jsonify({"success": False, "error": "no message"}), 400
     
     #update messages associated
     try:
@@ -42,23 +42,20 @@ def send():
         mailbox_send_id = None
     #validity
     if mailbox_id is None:
-        return jsonify({"error": "no recipient fingerprint match"}), 400
+        return jsonify({"success": False, "error": "no recipient fingerprint match"}), 400
     if mailbox_send_id is None:
-        return jsonify({"error": "no sender fingerprint match"}), 400
-
-    
-
-    #dont need this
-    #mail = Message.query.filter_by(mailbox_id=mailbox_id)
+        return jsonify({"success": False, "error": "no sender fingerprint match"}), 400
     
     try:
         message_ob = Message(message=message_t,sender_fingerprint=sender_fingerprint, mailbox_id=mailbox_id)
         db.session.add(message_ob)
         db.session.commit()
-    except DBAPIError:
-        return jsonify({"error": "message failed to add to messages"}), 400
-    
-    return jsonify({"success": True, "error": None})
 
-#messages in a mailbox instance is where message is added
-#to what I have now implies message not within mailbox needs modification
+        return jsonify({"success": True, data: None}), 201
+    except DBAPIError:
+        return jsonify({"success": False, "error": "message failed to add to messages"}), 400
+    
+    # Catch all; there was an unknown error
+    except BaseException as e:
+        return jsonify({"success": False, "error": repr(e)}), 500
+
