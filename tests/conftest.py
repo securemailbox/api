afb27b151661604ed3ccf0503b3391516ce3cba9
samgomena <samgomena@protonmail.com>
2020-05-15 13:03:20 -0700

    fix: Clean database before each test; fix failing new registration test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 import securemailbox
+from securemailbox.models import Message, Mailbox
 
 
 @pytest.fixture
@@ -9,3 +10,13 @@ def client():
 
     with securemailbox.app.test_client() as client:
         yield client
+
+# Force fixture to be used if not explicitly imported
+@pytest.fixture(autouse=True)
+def clean_database():
+    # Delete all messages first so mailbox doesn't fail on cascade
+    Message.query.delete()
+    # Delete all mailboxes
+    Mailbox.query.delete()
+
+    yield
